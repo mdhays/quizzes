dd702f88401bd380f9300bdfeb1ea649d8b55ec7
mdhays <mdhays1995@gmail.com>
2015-10-20 13:47:08 -0500

    turning in quiz

---
+++ quiz.js
@@ -0,0 +1,17 @@
+var emptyString = "";
+
+var two = 2;
+
+var fruitArray = [];
+fruitArray.push("apple", "orange", "pear", "grapes", "tomato", "avocado", "watermelon", "mango", "strawberry", "banana");
+console.log("fruitArray", fruitArray)
+
+for (var i = 0; i < fruitArray.length; i++1) {
+	
+}
+
+
+var fruitList = document.getElementById("fruit-list");
+console.log("fruitList", fruitList);
+
+
